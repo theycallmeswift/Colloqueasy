b0bfc8420b711546259855ebc5f2d7bb7025caf9
Mike Swift <theycallmeswift@gmail.com>
2011-12-06 00:47:50 -0500

    And... I can't type.

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -7,7 +7,7 @@ return array(
   'default' => array(
     'connection'  => array(
       'hostname'   => $_SERVER['DB1_HOST'],
-      'port'       => $_SERVER['BD1_PORT'],
+      'port'       => $_SERVER['DB1_PORT'],
       'database'   => $_SERVER['DB1_NAME'],
       'username'   => $_SERVER['DB1_USER'],
       'password'   => $_SERVER['DB1_PASS'],
