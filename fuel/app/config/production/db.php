7d28d90ef629534d9f1cf956bfae402e2c3105e4
Mike Swift <theycallmeswift@gmail.com>
2011-12-06 00:46:40 -0500

    2 hours of wasted time later, and I realize I'm an asshole.

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -6,7 +6,8 @@
 return array(
   'default' => array(
     'connection'  => array(
-      'host'       => $_SERVER['DB1_HOST'],
+      'hostname'   => $_SERVER['DB1_HOST'],
+      'port'       => $_SERVER['BD1_PORT'],
       'database'   => $_SERVER['DB1_NAME'],
       'username'   => $_SERVER['DB1_USER'],
       'password'   => $_SERVER['DB1_PASS'],
